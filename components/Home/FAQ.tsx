5cf0561ce2be8753548fbd740a470c1b609bce68
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-09-18 15:20:09 +0600

    Added types

--- components/Home/FAQ.tsx
+++ components/Home/FAQ.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { IFaqList } from "@/interface";
+import { faqsList } from "@/lib/data";
+import { FC, useRef, useState } from "react";
 
 const FaqsCard = (props) => {
   const answerElRef = useRef();
@@ -67,29 +69,6 @@ const FaqsCard = (props) => {
 };
 
 export const FAQ = () => {
-  const faqsList = [
-    {
-      q: "What is My Business Union platform?",
-      a: "My Business Union Platform is a platform where you will be able to enhance your performance through learning As you show your talents on Facebook, Instagram and tiktok etc like that you could able to show your talents in Yesnet Digital E-learning Platform as it is a digital marketing platform where you can learn. Besides learning you will be able to improve your work ability or performance.",
-    },
-    {
-      q: "Do we need any admission fees ?",
-      a: "Yes you need to pay admission fees for taking the course , product or services",
-    },
-    {
-      q: "Can we do this from the comfort of our home ?",
-      a: "Yes you can take this course or services from your home only because it's is a online process",
-    },
-    {
-      q: "What kind of documents and gadgets do we need to do this Course ?",
-      a: "There's not much requirements you will need for doing this Courses you just need an electronic device like a mobile or a laptop, a steady internet connection.",
-    },
-    {
-      q: "Is this a part-time or a full-time Work?",
-      a: "It's not a work or its not a job it's only a learning and earning process you need to join here as a learner besides of learning you will be able to earn selling some courses goods or services",
-    },
-  ];
-
   return (
     <section className="leading-relaxed max-w-screen-xl mt-12 mx-auto px-4 md:px-8">
       <div className="space-y-3 text-center">
