207fdd4171d65c08394ce3b2cd43100005df70bc
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-18 21:31:21 +0600

    FAQ stye done

--- components/Home/FAQ.tsx
+++ components/Home/FAQ.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { faqsList } from "@/lib/data";
-import { Container, MainContainer, Title } from "@/universal";
+import { Container, Label, MainContainer, Title } from "@/universal";
 import { FC } from "react";
 import { FaqsCard } from "./FaqsCard";
 
 export const FAQ: FC = () => (
-  <MainContainer>
-    <Container>
+  <MainContainer className="my-10 lg:my-20">
+    <Container className="text-center flex flex-col justify-center items-center gap-5 lg:gap-10">
       <Title variant="H1">Frequently Asked Questions</Title>
-      <p className="text-gray-600 max-w-lg mx-auto text-lg">
+      <Label className="max-w-2xl">
         Answered all frequently asked questions, Still confused? feel free to
         contact us.
-      </p>
-      <div className="mt-14 w-full md:max-w-5xl mx-auto">
+      </Label>
+      <div className="mt-2 w-full md:max-w-5xl mx-auto">
         {faqsList.map(({ ...faq }, idx) => (
           <FaqsCard key={idx} {...faq} />
         ))}
