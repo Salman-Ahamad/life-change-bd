a94922f98d03e6246da3c36c4cd584893a7ad520
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-21 14:59:18 +0600

    ImageUpload setup doen

---
+++ lib/assets/img/index.ts
@@ -0,0 +1,6 @@
+import popular1 from "./popular-1.jpg";
+import popular2 from "./popular-2.jpg";
+import popular3 from "./popular-3.jpg";
+import popular4 from "./popular-4.jpg";
+
+export { popular1, popular2, popular3, popular4 };
--- lib/assets/index.ts
+++ lib/assets/index.ts
@@ -1,10 +1,11 @@
 import banner from "./banner.jpg";
+import how2buy from "./how2buy.png";
 import loginBanner from "./login-banner.webp";
 import meetingilustration from "./shape-2.png";
-import how2buy from "./how2buy.png";
 
 export * from "./card";
 export * from "./icons";
+export * from "./img";
 export * from "./logo";
 
-export { banner, loginBanner, meetingilustration, how2buy };
+export { banner, how2buy, loginBanner, meetingilustration };
