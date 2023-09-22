ee07588136a10919580fbb58a8cb69aa8624b177
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-22 11:34:10 +0600

    Forgot Password

--- app/(without header footer)/(auth)/forgot-password/page.tsx
+++ app/(without header footer)/(auth)/forgot-password/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { ForgotPasswordForm } from "@/components";
+import { Container } from "@/universal";
 import { FC } from "react";
 
 const ForgotPassword: FC = () => (
-  <main className="h-screen flex justify-between items-center">
-    <section className="w-full px-5 lg:px-0 lg:w-[50vw] max-w-[370px] mx-auto">
+  <section className="h-screen flex justify-between items-center">
+    <Container className="w-full px-5 lg:px-0 lg:w-[50vw] max-w-[370px] mx-auto">
       <ForgotPasswordForm />
-    </section>
-  </main>
+    </Container>
+  </section>
 );
 
 export default ForgotPassword;
