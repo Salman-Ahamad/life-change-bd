650593afea9d8b6721910db480f2e68a527beff3
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-09-24 18:23:24 +0600

    added sample database schema

---
+++ lib/dataBase.ts
@@ -0,0 +1,85 @@
+// If we user mongoose, then we can write DB schema like this.
+
+// User Model
+const userSchema = {
+  phone: "",
+  email: "",
+  password: "",
+  role: ["User", "Admin", "SuperAdmin"],
+  googleId: "",
+  profilePicture: "",
+
+  // Personal Details
+  firstName: "",
+  lastName: "",
+  whatsAppNo: "",
+  language: "",
+  gender: ["Male", "Female", "Other"],
+  country: "",
+  division: "",
+  city: "",
+  address: "",
+  zipcode: "",
+  dateOfBirth: "",
+  createdAt: "",
+
+  // Points Model related information: Keep in separate table
+  points: 0,
+
+  // Referral related information: Keep in separate table
+  refarrel: [{ id: "", createdAt: "" }],
+
+  // Photo-zone related information: Keep in separate table
+  posts: [{ id: "", text: "", photoUrl: "", createdAt: "" }],
+  likes: [{ id: "" }],
+
+  // Course related information: Keep in separate table
+  course: [{ id: "", status: "" }],
+};
+
+// Role Model --- Need more details or we may just use this as enum only
+const roleSchema = {
+  name: ["User", "Admin", "SuperAdmin"],
+  permissions: [String],
+};
+
+// Points Transaction Model
+const pointsTransactionSchema = {
+  user: "id",
+  type: ["Earn", "Withdraw"],
+  method: ["bCash", "Nagad"],
+  requestTime: "",
+  amount: 0,
+  status: ["Pending", "Success", "Failed"],
+  message: "",
+  createdAt: "",
+};
+
+// Community Post Model
+const photoZoneSchema = {
+  author: "id",
+  imageUrl: "",
+  text: "",
+  likes: [{ user: "id" }],
+  createdAt: "",
+};
+
+// Referral Model
+const referralSchema = {
+  referrer: "id",
+  referredUser: "id",
+  pointsEarned: 0,
+  createdAt: "",
+};
+
+// Course Model
+const courseSchema = {
+  courseName: "",
+  courseCode: "",
+  courseDescription: "",
+  coursePrice: 0,
+  courseDuration: 0,
+  courseStatus: ["Active", "Completed"],
+  enrolled: ["id"],
+  certificates: [""],
+};
