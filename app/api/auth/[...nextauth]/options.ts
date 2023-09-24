3b55ba2da6a9ed48912a49c321e5a47f4bf47ba8
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-09-24 12:56:14 +0600

    Update auth setting and add different approach

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -3,15 +3,17 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 export const options: NextAuthOptions = {
+  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+
     CredentialsProvider({
       name: "credentials",
       credentials: {
-        username: {
+        phone: {
           label: "Phone",
           type: "tel",
           placeholder: "Your Phone Number",
@@ -21,10 +23,21 @@ export const options: NextAuthOptions = {
           type: "password",
         },
       },
+
       async authorize(credentials) {
+        // Check if the Password and user id entered
+        // This credentials.password will hassed using "bcrypt" from loging page
+        if (!credentials?.phone || !credentials.password) {
+          console.log("Please enter Phone number and Password!");
+          // throw new Error("Please enter Phone number and Password!");
+          return null;
+        }
+
+        // Now check the User id exist in the database
+        // const user = check the User id exist in the database
         const user = {
           id: "1",
-          username: "01819062270",
+          phone: "01819062270",
           password: "123456",
           email: "amirhossain.limon@gmail.com",
           image:
@@ -32,8 +45,82 @@ export const options: NextAuthOptions = {
           name: "Md Amir Hossain",
         };
 
+        // Now check with demo data. This will change when DB added
+        if (user.phone !== credentials.phone) {
+          console.log("Invalid User Id!");
+          // throw new Error("Invalid User Id!");
+          return null;
+        }
+
+        // Check if Password match
+        // This credentials.password will checked using "bcrypt" from loging page
+        // const passwordMatch = await bcrypt.compaer(
+        //   credentials.password,
+        //   user.password
+        // );
+        const passwordMatch = user.password === credentials.password;
+
+        // Now check the Password and user id with server data
+        if (!passwordMatch) {
+          console.log("Invalid Password!");
+          // throw new Error("Invalid Password!");
+          return null;
+        }
+
         return user;
       },
     }),
   ],
+  // ...add CUSTOM PAGE here
+  pages: {
+    signIn: "/user/login",
+    error: "/user/login",
+  },
+
+  // callbacks: {
+  //   async signIn({ user, account, profile }) {
+  //     // User: came from the "authorize" function in credential
+  //     // Account: {providerAccountId: undefined, type: 'credentials', provider: 'credentials'}
+  //     // Profile: Only return when login with provider (Google).
+
+  //     if (account.provider === "google") {
+  //       const { Email, id } = profile;
+
+  //       const existingUser = "await"; //Check if the data is exist in the database
+
+  //       if (!existingUser) {
+  //         // Link the Google account with the current user
+  //         // You should have access to the current user's ID in the `user` object
+  //         // Update DATABASE with the new user's ID
+  //       }
+  //     }
+  //     return true;
+  //   },
+
+  //   async jwt({ token, user }) {
+  //     if (user) {
+  //       token.modify = "Test Token";
+  //       // Include Google authentication info in the JWT if available
+  //       if (user.googleId) {
+  //         token.googleId = user.googleId;
+  //       }
+  //     }
+
+  //     return token;
+  //   },
+
+  //   async session({ session, token }) {
+  //     if (token.googleId) {
+  //       // Include Google authentication info in the session
+  //       session.user.googleId = token.googleId;
+  //     }
+  //     return session;
+  //   },
+  // },
+
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
 };
