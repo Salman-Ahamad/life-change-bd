b76f470bc13f748a97f279b949477a8e2b9ec549
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-25 11:50:35 +0600

    clean up

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,14 @@ import { NextResponse } from "next/server";
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(request: NextRequestWithAuth) {
-    // console.log(request.nextUrl.pathname)
-    // console.log(request.nextauth.token)
+    console.log(request.nextUrl.pathname);
+    console.log(request.nextauth.token);
 
     if (
-      request.nextUrl.pathname.startsWith("/extra") &&
+      request.nextUrl.pathname.startsWith("/user/inactive") &&
       request.nextauth.token?.role !== "admin"
     ) {
+      console.log(request.nextUrl.pathname);
       return NextResponse.rewrite(new URL("/denied", request.url));
     }
 
@@ -32,4 +33,4 @@ export default withAuth(
 
 // Applies next-auth only to matching routes - can be regex
 // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = { matcher: ["/extra", "/client", "/dashboard"] };
+export const config = { matcher: ["/user", "/client", "/dashboard"] };
