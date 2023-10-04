ce3fdfc58f2f49455fe560769698e234c9c0ce27
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-10-04 17:03:37 +0600

    clean up

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -16,7 +16,11 @@ export const GET = async () => {
     // Get Current User
     const { id, role } = await getCurrentUser();
 
-    if (role !== UserRole.active && role !== UserRole.admin) {
+    if (
+      role !== UserRole.active &&
+      role !== UserRole.inactive &&
+      role !== UserRole.admin
+    ) {
       return ApiResponse(401, "Denied❗unauthorized 😠😡😠");
     }
 
