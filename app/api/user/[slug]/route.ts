d724ca9b0a6ea2825352f1d09299c56833a44cd5
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-10-10 10:57:58 +0600

    single id get

---
+++ app/api/user/[slug]/route.ts
@@ -0,0 +1,57 @@
+import { connectDb } from "@/config";
+import { ISlugParams } from "@/interface";
+import { UserRole } from "@/lib";
+import { User } from "@/models";
+import { ApiResponse } from "@/utils";
+import getCurrentUser from "@/utils/actions/getCurrentUser";
+import { NextRequest } from "next/server";
+
+connectDb();
+
+export const GET = async (req: Request, { params }: ISlugParams) => {
+  try {
+    const id = params.slug;
+
+    // Get Current User
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+      return ApiResponse(404, "User not found❗");
+    }
+
+    if (currentUser.role !== UserRole.admin) {
+      return ApiResponse(401, "Denied❗unauthorized 😠😡😠");
+    }
+
+    const user = await User.findOne({ _id: id })
+      .populate("courses")
+      .select("-password");
+
+    return ApiResponse(200, "User get successfully 👌", user);
+  } catch (error: any) {
+    return ApiResponse(400, error.message);
+  }
+};
+
+export const PATCH = async (req: NextRequest) => {
+  try {
+    const updatedData = await req.json();
+
+    // Get Current User
+    const user = await getCurrentUser();
+
+    if (!user) {
+      return ApiResponse(404, "User not found❗");
+    } else if (!user.role) {
+      return ApiResponse(401, "Denied❗ unauthorized user 😠😡😠");
+    }
+
+    const result = await User.updateOne({ _id: user.id }, updatedData, {
+      new: true,
+    });
+
+    return ApiResponse(200, "User update successfully 🛠️✅", result);
+  } catch (error: any) {
+    return ApiResponse(400, error.message);
+  }
+};
