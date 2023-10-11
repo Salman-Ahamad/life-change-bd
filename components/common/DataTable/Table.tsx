f74f2aec7212ba94692f650495a58dd0a89c1459
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-10-11 17:20:36 +0600

    clena up

--- components/common/DataTable/Table.tsx
+++ components/common/DataTable/Table.tsx
@@ -16,26 +16,18 @@ const Tbody = ({ title }: ITHeader) => (
 );
 
 export interface ITable {
-  title?: string;
   tableHeaders: string[];
   dataProperties: string[];
   tableData: IAllRefer[];
 }
 
 export const Table: FC<ITable> = ({
-  title,
   tableHeaders,
   dataProperties,
   tableData,
 }) => {
   return (
     <div className="max-w-screen-xl mx-auto p-4 md:p-8">
-      {title && (
-        <h3 className="text-gray-800 text-xl font-bold sm:text-2xl max-w-lg">
-          {title}
-        </h3>
-      )}
-
       <div className="mt-8 shadow-sm border rounded-lg overflow-x-auto">
         <table className="w-full table-auto text-sm text-left">
           <thead className="bg-gray-50 text-gray-600 font-medium border-b">
