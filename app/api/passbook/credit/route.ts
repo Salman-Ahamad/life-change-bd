7b270d8a39ca5cf3b42cd933d859a47b2ebc7f53
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-10-18 20:11:13 +0600

    Update debit rout post method

--- app/api/passbook/credit/route.ts
+++ app/api/passbook/credit/route.ts
@@ -1,6 +1,8 @@
-import { Withdrawal } from "@/models";
+import { User, Withdrawal } from "@/models";
+import { Earning } from "@/models/earnings";
 import { ApiResponse } from "@/utils";
 import getCurrentUser from "@/utils/actions/getCurrentUser";
+import { NextRequest } from "next/server";
 
 export const GET = async () => {
   try {
@@ -11,9 +13,40 @@ export const GET = async () => {
       return ApiResponse(404, "User not found❗");
     }
 
-    const result = await Withdrawal.find({ userId: user.id });
+    const result = await Earning.find({ userId: user.id });
 
-    return ApiResponse(200, "Withdrawal data get successfully 👌", result);
+    return ApiResponse(200, "Earning data get successfully 👌", result);
+  } catch (error: any) {
+    return ApiResponse(400, error.message);
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { userId, amount, comments } = await req.json();
+    // Get Current User
+    const user = await getCurrentUser();
+
+    if (!user) {
+      return ApiResponse(404, "User not found❗");
+    }
+    const amountToNumber = Number(amount);
+
+    // await User.findOneAndUpdate(
+    //   { _id: user.id },
+    //   { $inc: { balance: -amountToNumber } },
+    //   { new: true }
+    // );
+
+    // const option = {
+    //   userId: user.id,
+    //   amount: amountToNumber,
+    //   ...withdrawalData,
+    // };
+
+    const result = await Earning.create({ userId, amount, comments });
+
+    return ApiResponse(200, "withdraw Request successfully 👌", result);
   } catch (error: any) {
     return ApiResponse(400, error.message);
   }
