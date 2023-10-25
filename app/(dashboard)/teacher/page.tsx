be77b93eb2458053541567e85f30a92d3d74f9b2
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-10-25 22:12:56 +0600

    Update meet link change

--- app/(dashboard)/teacher/page.tsx
+++ app/(dashboard)/teacher/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { Header } from "@/components";
-
 import { ChangeMeetingLink } from "@/components/SubAdmin/ChangeMeetingLink";
-import { useCurrentUser } from "@/hooks";
+import { updateData, useCurrentUser, useGetData } from "@/hooks";
 import { INavItem } from "@/interface";
-import { Container, Title } from "@/universal";
+import { Button, Container, Title } from "@/universal";
+import { useState } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 
 const navData: INavItem[] = [
@@ -24,7 +24,15 @@ const navData: INavItem[] = [
 ];
 
 const SubAdmin = () => {
+  const [welcomeMeetLink, setWelcomeMeetLink] = useState<string>("");
   const user = useCurrentUser(true);
+  useGetData("config/welcome-class", setWelcomeMeetLink, true);
+
+  const handleChangeMeetingLink = async () => {
+    updateData("config/welcome-class", { meetLink: welcomeMeetLink }).then(() =>
+      window.location.reload()
+    );
+  };
 
   return (
     <main>
@@ -34,6 +42,32 @@ const SubAdmin = () => {
         <Title variant="H3">Welcome to Life Change Bd</Title>
       </div>
       <Container className="flex flex-col-reverse lg:flex-row justify-center items-center gap-10 w-full py-12 px-6 mx-auto">
+        <div className="flex items-end justify-center">
+          <div className="flex flex-col gap-1">
+            <p className="pb-12">
+              Current Welcome Class Link:{" "}
+              <span className="font-semibold">{welcomeMeetLink}</span>
+            </p>
+            <label className="pl-1.5">Please enter meet ID</label>
+            <div className="flex gap-2.5">
+              <input
+                type="text"
+                onChange={(e) => setWelcomeMeetLink(e.target.value)}
+                className="outline-none text-black text-base md:text-lg max-w-xs border border-primary rounded-[5px] py-1 px-2"
+              />
+              <Button
+                variant="secondary"
+                className="py-[7px] lg:py-2.5 px-3"
+                onClick={() => {
+                  handleChangeMeetingLink();
+                }}
+              >
+                Save
+              </Button>
+            </div>
+          </div>
+        </div>
+
         {user?.settings?.course ? (
           <ChangeMeetingLink courseSlug={user?.settings?.course} />
         ) : (
---
+++ app/api/config/welcome-class/route.ts
@@ -0,0 +1,33 @@
+import { UserRole } from "@/lib";
+import { AppConfig } from "@/models";
+import { ApiResponse } from "@/utils";
+import getCurrentUser from "@/utils/actions/getCurrentUser";
+import { NextRequest } from "next/server";
+
+export const PATCH = async (req: NextRequest) => {
+  try {
+    const { meetLink } = await req.json();
+
+    // Get Current User
+    const user = await getCurrentUser();
+
+    if (!user) {
+      return ApiResponse(404, "User not found❗");
+    } else if (user.role !== UserRole.admin || user.role !== UserRole.teacher) {
+      return ApiResponse(401, "Denied❗unauthorized 😠😡😠");
+    }
+
+    const result = await AppConfig.updateOne(
+      { for: UserRole.admin },
+      { "support.welcomeClass": meetLink },
+      {
+        new: true,
+      }
+    );
+
+    return ApiResponse(200, "Config update successfully 🛠️✅", result);
+  } catch (error: any) {
+    console.log("Error in Server: ", error);
+    return ApiResponse(400, error.message);
+  }
+};
